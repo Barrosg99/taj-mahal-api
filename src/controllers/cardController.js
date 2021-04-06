fe55d7ee3a87cbb6b77198c11e38f2d6e94199e4
Gabriel Barros <barros.fontes@unesp.br>
2021-04-06 17:40:21 -0300

    fix: orderBy

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -24,7 +24,7 @@ class CardController {
         model: User,
         attributes: ['nickname'],
       },
-      order: ['createdAt', 'DESC'],
+      order: [['createdAt', 'DESC']],
     });
   }
 }
